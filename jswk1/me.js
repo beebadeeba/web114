8ae3a5ecac235df64af9622655c5b2ff496b8a01
beebadeeba <bradycorbinn@icloud.com>
2025-10-14 10:08:34 -0500

    Add files via upload

---
+++ jswk1/me.js
@@ -0,0 +1,17 @@
+//* Brady Corbin - 10/14/2025 *//
+
+let name="Brady Corbin";
+
+let favoriteColor="green";
+
+let hobby="guitar";
+
+let favoriteFood="sushi";
+
+let favoriteAnimal="raccoon";
+
+
+console.log("My favorite color is "+ favoriteColor)
+console.log("My current hobby is "+ hobby)
+console.log("My favorite food is "+ favoriteFood)
+console.log("My favorite animal is "+ favoriteAnimal)
