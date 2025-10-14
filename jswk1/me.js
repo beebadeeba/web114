93be63df4714a68537a8a7135ec9375ca31f4898
beebadeeba <bradycorbinn@icloud.com>
2025-10-14 10:32:48 -0500

    ok im done

--- jswk1/me.js
+++ jswk1/me.js
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-
-<html lang="en">
-
-<head>
-
-  <meta charset="utf-8" />
-  <title>Me</title>
-
-</head>
-<body>
-
-  <script>
     let name = "Brady Corbin";
 
     let favoriteColor = "green";
@@ -25,7 +12,4 @@
     console.log("My current hobby is " + hobby);
     console.log("My favorite food is " + favoriteFood);
     console.log("My favorite animal is " + favoriteAnimal);
-  </script>
-
-</body>
-</html>
+ 
